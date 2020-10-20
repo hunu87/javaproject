42383c90f0f416f50c22760e2c0f4a584f21d0c3
ihyeon-u <ihyeon-u@localhost>
2020-10-20 21:13:17 +0900

    20201020 java8 project start

---
+++ JavaProject/src/java8to14.java
@@ -0,0 +1,21 @@
+
+public class java8to14 {
+		
+	public static void main(String[] args) {
+		
+		// 익명 내부 클래스
+		/*
+		SimpleInterface funSimpleInterface = new SimpleInterface() {
+			@Override
+			public void action() {
+				System.out.println("익명 클래스, 안녕하세요!");
+			}
+		};
+		*/
+		
+		// lambda
+		SimpleInterface funSimpleInterface = () -> System.out.println("람다식, 안녕하세요!");
+		
+		funSimpleInterface.action();
+	}
+}
