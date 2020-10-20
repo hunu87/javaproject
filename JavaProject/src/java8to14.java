53a24794ef6ce3dd0e8c425c0780f83d26bb41d6
ihyeon-u <ihyeon-u@localhost>
2020-10-20 22:33:19 +0900

    20201020 Supplier 예제 작성

--- JavaProject/src/java8to14.java
+++ JavaProject/src/java8to14.java
@@ -1,3 +1,4 @@
+import java.util.function.Supplier;
 
 public class java8to14 {
 		
@@ -28,8 +29,18 @@ public class java8to14 {
 		};
 		*/
 		
-		SimpleConsumerInterface consumerInterface = (s) -> System.out.println("람다식, " + s + "님, 안녕하세요!");
+		/* 람다식이 하나의 실행문이라면, 중괄호를 생략할 수 있다. */
+		SimpleConsumerInterface consumerInterface = 
+				// (s) -> System.out.println("람다식, " + s + "님, 안녕하세요!");
+				s -> System.out.println("람다식, " + s + "님, 안녕하세요!");	// 하나의 매개변수만 존재 한다면, () 생략할 수 있다.
 		
 		consumerInterface.accept("이현우");
+		
+		/* Supplier 예시 
+		 * 인자는 받지 않고, 리턴 타입만 존재한다.
+		 * get 메소드는 항상 동일한 데이터를 반환
+		 * */
+		Supplier<String> supplier = () -> "Supplier 예시, 반환 데이터 존재";
+		System.out.println(supplier.get());
 	}
 }
