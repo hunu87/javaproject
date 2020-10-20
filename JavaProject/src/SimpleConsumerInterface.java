ff6c086488bb54d0818693c735d714cef14a4d59
ihyeon-u <ihyeon-u@localhost>
2020-10-20 21:49:09 +0900

    20201020 lambda practice

---
+++ JavaProject/src/SimpleConsumerInterface.java
@@ -0,0 +1,4 @@
+
+public interface SimpleConsumerInterface {
+	void accept(String s);
+}
--- JavaProject/src/java8to14.java
+++ JavaProject/src/java8to14.java
@@ -18,5 +18,18 @@ public class java8to14 {
 		
 		funSimpleInterface.action();
 		
+		// 익명 내부 클래스8
+		/*
+		SimpleConsumerInterface consumerInterface = new SimpleConsumerInterface() {
+			@Override
+			public void accept(String s) {
+				System.out.println("익명 클래스, " + s + "님, 안녕하세요!");
+			}
+		};
+		*/
+		
+		SimpleConsumerInterface consumerInterface = (s) -> System.out.println("람다식, " + s + "님, 안녕하세요!");
+		
+		consumerInterface.accept("이현우");
 	}
 }
