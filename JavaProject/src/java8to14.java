1300bd18f93933daaa229a0d491ab0522764eae5
ihyeon-u <ihyeon-u@localhost>
2020-10-25 22:08:07 +0900

    20201025 Consumer, Function, Predicate 예시 작성

--- JavaProject/src/java8to14.java
+++ JavaProject/src/java8to14.java
@@ -1,4 +1,7 @@
 import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class java8to14 {
 		
@@ -42,5 +45,31 @@ public class java8to14 {
 		 * */
 		Supplier<String> supplier = () -> "Supplier 예시, 반환 데이터 존재";
 		System.out.println(supplier.get());
+		
+		/* Consumer<T> 예시 
+		 * T 타입의 인자는 받고, 리턴은 하지 않는다.
+		 * 즉, 전달받은 매개변수를 소비하고 끝낸다.
+		 * */
+	     Consumer<String> consumer = s -> System.out.println(s);
+	     consumer.accept("Consumer 예시, 매개변수는 있지만 반환 데이터 없음");
+	     
+	     /*
+	      * Consumer, 메소드 레퍼런스 
+	      * */
+	     Consumer<String> consumer2 = System.out::println;
+	     consumer2.accept("Consumer 예시2, 메소드 레퍼런스");
+	     
+	     /* Function<T,R> 예시 
+			 * 매개변수가 있고, 리턴 값도 있는 경우이다.
+			 * */
+	    Function<String, String> function = String::toUpperCase;
+	    System.out.println(function.apply("eddy"));
+	    
+	    /* Predicate<T> 예시 
+		 * 매개변수가를 받고, boolean 리턴한다.
+		 * Function<T, Boolean> 와 같은 기능을 한다고 생각하면 됨
+		 * */
+	    Predicate<String> predicate = s -> s.startsWith("e");
+	    System.out.println("e로 시작하면 true 리턴 : " + predicate.test("eddy"));
 	}
 }
